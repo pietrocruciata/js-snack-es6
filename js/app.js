20130d0cd63dec8e6e12ab23b28d7edecc0bbdee
pietrocruciata <cruciata9748@gmail.com>
2024-03-20 17:04:33 +0100

    found: the lowest weight bike

--- js/app.js
+++ js/app.js
@@ -94,3 +94,100 @@ console.log(studentsGoods);
 //CREARE UNA LISTA DI TUTTI GLI STUDENTI CHE HANNO DEI VOTI SUPERIORI A 70 E ID SUPERIORE A 120
 let studentsGoodsId = students.filter((el) => el.grades > 70 && el.id > 120)
 console.log(studentsGoodsId);
+
+
+
+/////SNACK 3
+
+// CREARE UN ARRAY DI BICI
+const bicycles = [
+    {
+        nome: 'hiland',
+        peso: 10
+    },
+
+    {
+        nome: 'galano',
+        peso: 8
+    },
+
+    {
+        nome: 'MU',
+        peso: 9
+    },
+
+    {
+        nome: 'kabon',
+        peso: 6
+    },
+
+    {
+        nome: 'pinarello',
+        peso: 7
+    },
+
+
+]
+
+//TROVARE LA BICI CON IL PESO MINORE
+
+let lowestWeightFound = bicycles[0]
+
+for ( let i = 1; i < bicycles.length; i++){
+    const currentWeight = bicycles[i]            
+    if(currentWeight.peso < lowestWeightFound.peso){
+        lowestWeightFound = currentWeight;
+    }
+}
+// STAMPARE IN CONSOLE LA BICI CON IL PESO MINORE
+
+console.log(lowestWeightFound);
+
+
+
+////SNACK 4
+//CREARE UN ARRAY DI OGGETTI DI SQUADRE DI CALCIO
+const footballClubs = [
+    {
+        nome: 'juve',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'napoli',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'palermo',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'milan',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+    {
+        nome: 'inter',
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+
+
+]
+
+
+//CAMBIARE I PUNTI FATTI E I FALLI SUBITI CON DEI NUMERI RANDOM
+
+const randomPoints = Math.floor(Math.random() * 115)
+const randomFauls = Math.floor(Math.random() * 180)
+
+
+
+//CREARE UN NUOVO ARRAY SOLO CON I NOMI DELLE SQUADRE E I FALLI SUBITI
+//STAMPARE IL NUOVO ARRAY IN CONSOLE
